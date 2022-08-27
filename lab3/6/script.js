6ba91a6c532bf9fc27c1b20252775d4780636653
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-08-27 22:35:35 +0700

    Update script.js

--- lab3/6/script.js
+++ lab3/6/script.js
@@ -1,30 +1,29 @@
 check = true;
-var j = 0;
 function reply_click(clicked_id) {
     let count = document.getElementById("count");
     let change = document.getElementById(clicked_id);
     if (check && change.innerHTML == "" && clicked_id != "reset") {
         change.innerHTML = "X";
         check = false;
-        j++;
-        count.innerHTML = "Turn: " + j;
+        count.innerHTML = "Turn: Player 'O'";
         change.style.borderColor = "#eee";
-        check_win();
     } else if (!check && change.innerHTML == "" && clicked_id != "reset") {
         change.innerHTML = "O";
         check = true;
-        j++;
-        count.innerHTML = "Turn: " + j;
+        count.innerHTML = "Turn: Player 'X'";
         change.style.borderColor = "#eee";
-        check_win();
     } else if (clicked_id == "reset") {
         reset();
     }
+    setTimeout(function(){check_win()}, 100)
 }
 
 function reset() {
-    j = 0;
-    count.innerHTML = "Turn: " + j;
+    if(check){
+        count.innerHTML = "Turn: Player 'X'";
+    } else{
+        count.innerHTML = "Turn: Player 'O'";
+    }
     box1.style.borderColor = "black";
     box2.style.borderColor = "black";
     box3.style.borderColor = "black";
@@ -50,28 +49,28 @@ function check_win() {
     if (box1.innerHTML == box4.innerHTML && box4.innerHTML == box7.innerHTML && box1.innerHTML != "" && box4.innerHTML != "" && box7.innerHTML != "") {
         alert("Player '" + box1.innerHTML + "' WIN!!");
         reset();
-    } else if(box2.innerHTML == box5.innerHTML && box5.innerHTML == box8.innerHTML && box2.innerHTML != "" && box5.innerHTML != "" && box8.innerHTML != ""){
+    } else if (box2.innerHTML == box5.innerHTML && box5.innerHTML == box8.innerHTML && box2.innerHTML != "" && box5.innerHTML != "" && box8.innerHTML != "") {
         alert("Player '" + box2.innerHTML + "' WIN!!");
-        reset()
-    } else if(box3.innerHTML == box6.innerHTML && box6.innerHTML == box9.innerHTML && box3.innerHTML != "" && box6.innerHTML != "" && box9.innerHTML != ""){
+        reset();
+    } else if (box3.innerHTML == box6.innerHTML && box6.innerHTML == box9.innerHTML && box3.innerHTML != "" && box6.innerHTML != "" && box9.innerHTML != "") {
         alert("Player '" + box3.innerHTML + "' WIN!!");
         reset();
-    } else if(box1.innerHTML == box2.innerHTML && box2.innerHTML == box3.innerHTML && box1.innerHTML != "" && box2.innerHTML != "" && box3.innerHTML != ""){
+    } else if (box1.innerHTML == box2.innerHTML && box2.innerHTML == box3.innerHTML && box1.innerHTML != "" && box2.innerHTML != "" && box3.innerHTML != "") {
         alert("Player '" + box1.innerHTML + "' WIN!!");
         reset();
-    } else if(box4.innerHTML == box5.innerHTML && box5.innerHTML == box6.innerHTML && box4.innerHTML != "" && box5.innerHTML != "" && box6.innerHTML != ""){
+    } else if (box4.innerHTML == box5.innerHTML && box5.innerHTML == box6.innerHTML && box4.innerHTML != "" && box5.innerHTML != "" && box6.innerHTML != "") {
         alert("Player '" + box4.innerHTML + "' WIN!!");
         reset();
-    } else if(box7.innerHTML == box8.innerHTML && box8.innerHTML == box9.innerHTML && box7.innerHTML != "" && box8.innerHTML != "" && box9.innerHTML != ""){
+    } else if (box7.innerHTML == box8.innerHTML && box8.innerHTML == box9.innerHTML && box7.innerHTML != "" && box8.innerHTML != "" && box9.innerHTML != "") {
         alert("Player '" + box7.innerHTML + "' WIN!!");
         reset();
-    } else if(box1.innerHTML == box5.innerHTML && box5.innerHTML == box9.innerHTML && box1.innerHTML != "" && box5.innerHTML != "" && box9.innerHTML != ""){
+    } else if (box1.innerHTML == box5.innerHTML && box5.innerHTML == box9.innerHTML && box1.innerHTML != "" && box5.innerHTML != "" && box9.innerHTML != "") {
         alert("Player '" + box1.innerHTML + "' WIN!!");
         reset();
-    } else if(box3.innerHTML == box5.innerHTML && box5.innerHTML == box7.innerHTML && box3.innerHTML != "" && box5.innerHTML != "" && box7.innerHTML != ""){
+    } else if (box3.innerHTML == box5.innerHTML && box5.innerHTML == box7.innerHTML && box3.innerHTML != "" && box5.innerHTML != "" && box7.innerHTML != "") {
         alert("Player '" + box3.innerHTML + "' WIN!!");
         reset();
-    } else if(box1.innerHTML != "" && box2.innerHTML != "" && box3.innerHTML != "" && box4.innerHTML != "" && box5.innerHTML != "" && box6.innerHTML != "" && box7.innerHTML != "" && box8.innerHTML != "" && box9.innerHTML != ""){
+    } else if (box1.innerHTML != "" && box2.innerHTML != "" && box3.innerHTML != "" && box4.innerHTML != "" && box5.innerHTML != "" && box6.innerHTML != "" && box7.innerHTML != "" && box8.innerHTML != "" && box9.innerHTML != "") {
         alert("Game Drawn!!");
         reset();
     }
