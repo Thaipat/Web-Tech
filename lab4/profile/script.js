e2f63a24971bbafa0d41cd99cc045d113ba8898f
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-08-29 19:57:16 +0700

    new

--- lab4/profile/script.js
+++ lab4/profile/script.js
@@ -3,3 +3,12 @@ function smoothScroll(clicked_id) {
         behavior: 'smooth'
     });
 }
+
+function moreFunction() {
+    let parent = document.getElementById("content");
+    let child = document.getElementById("child");
+    child.style.opacity = "0";
+    setTimeout(function(){parent.removeChild(child)}, 100);
+    let content = document.getElementById("content-text");
+    setTimeout(function(){content.style.opacity = "1";}, 300)
+}
