bd60890cb46952dba8c9309b39e8c0a6254cd575
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-08-29 20:13:00 +0700

    new

--- lab4/profile/script.js
+++ lab4/profile/script.js
@@ -9,6 +9,17 @@ function moreFunction() {
     let child = document.getElementById("child");
     child.style.opacity = "0";
     setTimeout(function(){parent.removeChild(child)}, 100);
+    setTimeout(function(){createContent(parent)}, 100);
+}
+
+function createContent(parent){
+    let para = document.createElement("h1");
+    let node = document.createTextNode("กดมาทะไมเรอะ!! หืมมม??")
+    let att = document.createAttribute("id")
+    att.value = "content-text"
+    para.appendChild(node);
+    para.setAttributeNode(att)
+    parent.appendChild(para);
     let content = document.getElementById("content-text");
     setTimeout(function(){content.style.opacity = "1";}, 300)
 }
