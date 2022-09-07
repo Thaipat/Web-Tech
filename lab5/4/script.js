bbbbc97b77584fea4cecf0f3193e73e4c337ef1a
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-09-07 13:38:25 +0700

    new

--- lab5/4/script.js
+++ lab5/4/script.js
@@ -9,9 +9,14 @@ request.open("GET", requestURL, true);
 request.send();
 
 function ExtractData(data) {
-    document.write("<body><div>")
+    document.write(`<body><div style="display:flex;justify-content:center;align-items:center;margin-top:11%">`)
     for (let i = 0; i < data.movies.showing.length; i++) {
-        document.write(`<div><img src="${data.movies.showing[i].poster}" style="width:150px"></div>`)
+        document.write(`<div style="margin:40px 10px;border: 2px solid #eee;padding: 0px 20px 15px 20px;border-radius: 5px;position:relative;min-width:170px">\
+        <img src="${data.movies.showing[i].poster}" style="width:150px;position:absolute;top:-10%;left:14%">\
+        <div style="width:150px;height:200px;margin-bottom:10px;"></div>\
+        <div style="display:flex;justify-content:center"><h5 style="margin:0px">${data.movies.showing[i].title_th}</h5></div>\
+        <div style="display:flex;justify-content:center;font-size:12px"><p>Release date : ${data.movies.showing[i].releasedate}</p></div>\
+        <button type="button" style="margin-left:10px;padding:5px 34px;background-color:#007bff;color:white;border-radius:5px;border: 0px;font-size:12px">เช็ครอบภาพยนต์</button></div>`)
     }
     document.write("</div></body>")
 }
