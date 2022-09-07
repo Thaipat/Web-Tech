9615f838cf6786efed376f472dbc364002933427
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-09-07 12:04:50 +0700

    new

--- lab5/4/script.js
+++ lab5/4/script.js
@@ -1,4 +1,4 @@
-let requestURL = 'countries.json';
+let requestURL = 'movies.json';
 let request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (request.readyState == 4 && request.status == 200) {
@@ -9,7 +9,9 @@ request.open("GET", requestURL, true);
 request.send();
 
 function ExtractData(data) {
+    document.write("<body><div>")
     for (let i = 0; i < data.movies.showing.length; i++) {
-
+        document.write(`<div><img src="${data.movies.showing[i].poster}" style="width:150px"></div>`)
     }
+    document.write("</div></body>")
 }
