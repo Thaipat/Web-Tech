72d70817f69db628359fc001c13393fb657264c1
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-10-03 02:11:13 +0700

    new

--- lab7/2/index.php
+++ lab7/2/index.php
@@ -1,12 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lab7/2</title>
 </head>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    table {
+        border: 1px solid black;
+        text-align: center;
+        height: 100px;
+
+    }
+
+    table th,
+    td {
+        border: 1px solid black;
+        font-size: 30px;
+        padding: 10px;
+    }
+    .head{
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
 <body>
-    
+    <div>
+        <div class="head">
+            <h1>JUNE 2022</h1>
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <th>Su</th>
+                    <th>Mo</th>
+                    <th>Tu</th>
+                    <th>We</th>
+                    <th>Th</th>
+                    <th>Fr</th>
+                    <th>Sa</th>
+                </tr>
+                <?php
+                $k = 1;
+                $check = false;
+                for ($i = 0; $i < 5; $i++) {
+                    for ($j = 0; $j < 7; $j++) {
+                        if ($j == 3) {
+                            $check = true;
+                        } else if (!$check) {
+                            echo "<td></td>";
+                        }
+                        if ($check && $k <= 30) {
+                            echo "<td>$k</td>";
+                            $k++;
+                        } else if ($check) {
+                            echo "<td></td>";
+                        }
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+        </div>
+    </div>
 </body>
+
 </html>
---
+++ lab7/4/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lab7/4</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    body{
+        display: grid;
+        grid-template-columns: 24% 24% 24% 24%;
+        grid-gap: 1%;
+    }
+    img{
+        width: 100%;
+        border-radius: 10px;
+        margin: 2%;
+    }
+</style>
+
+<body>
+    <?php
+    echo "<div>";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181394604494908/cat1.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181394973606019/cat2.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181395338506320/cat3.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181395678240828/cat4.png\">";
+    echo "</div>";
+    echo "<div>";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181396391284756/cat6.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181396869431356/cat7.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181397213351946/cat8.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181396034764810/cat5.png\">";
+    echo "</div>";
+    echo "<div>";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181397733462136/cat9.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181434773356634/cat11.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181435159224400/cat12.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181435549306941/cat13.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181435796758680/cat14.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181436102955078/cat15.png\">";
+    echo "</div>";
+    echo "<div>";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181398106738809/cat10.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181436430102568/cat16.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181436765655071/cat17.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181437348647023/cat18.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181437885534228/cat19.png\">";
+    echo "<img src=\"https://cdn.discordapp.com/attachments/884472367210967072/1026181438237843476/cat20.png\">";
+    echo "</div>";
+    ?>
+</body>
+
+</html>
