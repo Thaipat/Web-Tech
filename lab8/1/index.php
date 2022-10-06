39f4862e514433dcd95ea485b076b311328d9410
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2022-10-06 20:55:41 +0700

    new

--- lab8/1/index.php
+++ lab8/1/index.php
@@ -8,17 +8,27 @@
     <title>lab8/1</title>
 </head>
 <style>
+    form {
+        display: flex;
+        justify-content: center;
+        margin-top: 20%;
+    }
+
     input[type=number]::-webkit-inner-spin-button {
         -webkit-appearance: none;
     }
 
     .text-box {
-        position: relative;
         display: inline-block;
+        position: relative;
         border: 1px solid rgb(118, 118, 118);
-        padding: 9px 165px 9px 0px;
+        /* padding: 9px 165px 9px 0px; */
         border-radius: 2px;
-        top: 6px;
+        max-width: 165px;
+        min-width: 165px;
+        min-height: 20px;
+        max-height: 20px;
+        top: 7px;
     }
 
     button {
@@ -32,26 +42,30 @@
 
 <body>
     <form action="index.php" method="POST">
-        <label>From :</label>
-        <select id="from" name="from">
-            <option value="THB">THB</option>
-            <option value="JPY">JPY</option>
-            <option value="CNY">CNY</option>
-            <option value="SGD">SGD</option>
-            <option value="USD">USD</option>
-        </select>
-        <input type="number" name="money" id="money"><br>
-        <div><label>To : </label>
-            <select id="to" name="to">
-                <option value="THB">THB</option>
-                <option value="JPY">JPY</option>
-                <option value="CNY">CNY</option>
-                <option value="SGD">SGD</option>
-                <option value="USD">USD</option>
-            </select>
-            <div class="text-box" name="ans" id="ans"></div>
+        <div>
+            <div class="topp">
+                <label>From :</label>
+                <select id="from" name="from">
+                    <option value="THB">THB</option>
+                    <option value="JPY">JPY</option>
+                    <option value="CNY">CNY</option>
+                    <option value="SGD">SGD</option>
+                    <option value="USD">USD</option>
+                </select>
+                <input type="number" name="money" id="money"><br>
+            </div>
+            <div class="bottom" id="bottom"><label id="to_text">To : </label>
+                <select id="to" name="to">
+                    <option value="THB">THB</option>
+                    <option value="JPY">JPY</option>
+                    <option value="CNY">CNY</option>
+                    <option value="SGD">SGD</option>
+                    <option value="USD">USD</option>
+                </select>
+                <div class="text-box" name="ans" id="ans"></div>
+            </div>
+            <button type="submit" id="button">Convert</button>
         </div>
-        <button type="submit">Convert</button>
     </form>
     <?php
     $url = "http://10.0.15.20/lab8/restapis/currencyrate";
@@ -75,6 +89,9 @@
         document.getElementById("from").value = "<?php echo $money_from ?>"
         document.getElementById("to").value = "<?php echo $money_to ?>"
         document.getElementById("money").value = "<?php echo $money ?>"
+        document.getElementById("to").style = "position:relative;top:7px;height:18.5px;"
+        document.getElementById("to_text").style = "position:relative;top:7px;"
+        document.getElementById("button").style = "position:relative;top:5px"
     </script>
 </body>
 
